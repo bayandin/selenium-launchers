22bcccfacdb1812f4478738221bb59fe04b0474f
Alexander Bayandin <a.bayandin@gmail.com>
2013-05-23 08:47:16 +0700

    Bumped to 2.33.0

--- start-linux.sh
+++ start-linux.sh
@@ -2,7 +2,7 @@
 
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-	java -jar bin/selenium-server-standalone-2.32.0.jar -port 4455 -Dwebdriver.chrome.driver="bin/linux/x86-64/chromedriver" -Dphantomjs.binary.path="bin/linux/x86-64/phantomjs"
+	java -jar bin/selenium-server-standalone-2.33.0.jar -port 4455 -Dwebdriver.chrome.driver="bin/linux/x86-64/chromedriver" -Dphantomjs.binary.path="bin/linux/x86-64/phantomjs"
 else
-	java -jar bin/selenium-server-standalone-2.32.0.jar -port 4455 -Dwebdriver.chrome.driver="bin/linux/x86/chromedriver" -Dphantomjs.binary.path="bin/linux/x86/phantomjs"
+	java -jar bin/selenium-server-standalone-2.33.0.jar -port 4455 -Dwebdriver.chrome.driver="bin/linux/x86/chromedriver" -Dphantomjs.binary.path="bin/linux/x86/phantomjs"
 fi
