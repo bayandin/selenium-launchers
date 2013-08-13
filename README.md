44a6e73ffad62d4de17ebb7ff39fa31ad3fc32e6
Alexander Bayandin <a.bayandin@gmail.com>
2013-08-13 11:22:14 +0700

    Update selenium to 2.25.0

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Selenium Server Port: 4455
 
 | App | Version |
 |:-----------|:-------------|
-| [Selenium Server](http://seleniumhq.org) | 2.34.0
+| [Selenium Server](http://seleniumhq.org) | 2.35.0
 | [Ghost Driver (PhantomJS)](http://phantomjs.org/) | 1.0.3 (1.9.1)
 | [Chrome Driver](https://code.google.com/p/chromedriver/) | 2.2
-| [IE Driver](https://code.google.com/p/selenium/downloads/list) | 2.34.0
+| [IE Driver](https://code.google.com/p/selenium/downloads/list) | 2.35.1
--- start-win.bat
+++ start-win.bat
@@ -1,5 +1,5 @@
 @echo off
 IF %processor_architecture% == AMD64 (
-	start java -jar bin\selenium-server-standalone-2.34.0.jar -port 4455 -Dwebdriver.chrome.driver="bin\win\x86-64\chromedriver.exe" -Dwebdriver.ie.driver="bin\win\x86-64\IEDriverServer.exe" -Dphantomjs.binary.path="bin\win\x86-64\phantomjs.exe")
+	start java -jar bin\selenium-server-standalone-2.35.0.jar -port 4455 -Dwebdriver.chrome.driver="bin\win\x86-64\chromedriver.exe" -Dwebdriver.ie.driver="bin\win\x86-64\IEDriverServer.exe" -Dphantomjs.binary.path="bin\win\x86-64\phantomjs.exe")
 IF %processor_architecture% == x86 (
-	start java -jar bin\selenium-server-standalone-2.34.0.jar -port 4455 -Dwebdriver.chrome.driver="bin\win\x86\chromedriver.exe" -Dwebdriver.ie.driver="bin\win\x86\IEDriverServer.exe" -Dphantomjs.binary.path="bin\win\x86\phantomjs.exe")
+	start java -jar bin\selenium-server-standalone-2.35.0.jar -port 4455 -Dwebdriver.chrome.driver="bin\win\x86\chromedriver.exe" -Dwebdriver.ie.driver="bin\win\x86\IEDriverServer.exe" -Dphantomjs.binary.path="bin\win\x86\phantomjs.exe")
