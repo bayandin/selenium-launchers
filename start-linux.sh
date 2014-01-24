ec04d921142810c8a43e5ccabd841c5e65dccbeb
Alexander Bayandin <a.bayandin@gmail.com>
2014-01-24 11:07:41 +0400

    x86-64 → x86_64

--- start-linux.sh
+++ start-linux.sh
@@ -4,7 +4,7 @@ MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
 java -jar bin/selenium-server-standalone-2.39.0.jar\
  -port 4455\
- -Dwebdriver.chrome.driver="bin/linux/x86-64/chromedriver"\
+ -Dwebdriver.chrome.driver="bin/linux/x86_64/chromedriver"\
  -Dwebdriver.chrome.logfile="chromedriver.log"\
  -Dphantomjs.binary.path="bin/linux/x86-64/phantomjs"
 else
--- start-win.bat
+++ start-win.bat
@@ -1,8 +1,7 @@
-@echo off
 IF %processor_architecture% == AMD64 (
 start java -jar bin\selenium-server-standalone-2.39.0.jar^
  -port 4455^
- -Dwebdriver.chrome.driver="bin\win\x86-64\chromedriver.exe"^
+ -Dwebdriver.chrome.driver="bin\win\x86_64\chromedriver.exe"^
  -Dwebdriver.chrome.logfile="chromedriver.log"^
  -Dwebdriver.ie.driver="bin\win\x86-64\IEDriverServer.exe"^
  -Dphantomjs.binary.path="bin\win\x86-64\phantomjs.exe"
