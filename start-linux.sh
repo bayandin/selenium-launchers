40f81770f861e620681d8673da5992336696c367
Alexander Bayandin <a.bayandin@gmail.com>
2014-03-07 10:19:48 +0400

    Convert start-[linux|mac].sh to 'alias' for start.sh

--- start-linux.sh
+++ start-linux.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-java -jar bin/selenium-server-standalone-2.40.0.jar\
- -port 4455\
- -Dwebdriver.chrome.driver="bin/linux/x86_64/chromedriver"\
- -Dwebdriver.chrome.logfile="chromedriver.log"\
- -Dphantomjs.binary.path="bin/linux/x86-64/phantomjs"
-else
-java -jar bin/selenium-server-standalone-2.40.0.jar\
- -port 4455\
- -Dwebdriver.chrome.driver="bin/linux/x86/chromedriver"\
- -Dwebdriver.chrome.logfile="chromedriver.log"\
- -Dphantomjs.binary.path="bin/linux/x86/phantomjs"
-fi
+BASEDIR=$(dirname $0)
+$BASEDIR/start.sh
