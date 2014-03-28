8c311d196c883e26def2699ae09198ead1b9a4ef
Alexander Bayandin <a.bayandin@gmail.com>
2014-03-28 09:27:26 +0400

    selenium server 2.41.0, iedriver 2.41.0

--- start.sh
+++ start.sh
@@ -15,8 +15,9 @@ fi
 # Add machine type to path
 DRIVER_PATH="$BASEDIR/$DRIVER_PATH/$MACHINE_TYPE"
 
-java -jar $BASEDIR/bin/selenium-server-standalone-2.40.0.jar\
+java -jar $BASEDIR/bin/selenium-server-standalone-2.41.0.jar\
  -port 4455\
  -Dwebdriver.chrome.driver="$DRIVER_PATH/chromedriver"\
  -Dwebdriver.chrome.logfile="chromedriver.log"\
- -Dphantomjs.binary.path="$DRIVER_PATH/phantomjs"
+ -Dphantomjs.binary.path="$DRIVER_PATH/phantomjs"\
+ -Dphantomjs.cli.args="--webdriver-logfile=phantomjs.log"
