1b1a48d1512ba651d9ff6fb326918cab0f3e40f6
Alexander Bayandin <a.bayandin@gmail.com>
2014-05-23 08:02:18 +0400

    selenium server 2.42.0

--- start.sh
+++ start.sh
@@ -5,19 +5,19 @@ MACHINE_TYPE=`uname -m`
 BASEDIR=$(dirname $0)
 
 if [ ${MACHINE_OS} == 'Linux' ]; then
- DRIVER_PATH="bin/linux"
+  DRIVER_PATH="bin/linux"
 elif [ ${MACHINE_OS} == 'Darwin' ]; then
- DRIVER_PATH="bin/mac"
+  DRIVER_PATH="bin/mac"
 else
- echo >&2 "Unknown *unix OS.  Aborting."; exit 1;
+  echo >&2 "Unknown *unix OS.  Aborting."; exit 1;
 fi
 
 # Add machine type to path
 DRIVER_PATH="$BASEDIR/$DRIVER_PATH/$MACHINE_TYPE"
 
-java -jar $BASEDIR/bin/selenium-server-standalone-2.41.0.jar\
- -port 4455\
- -Dwebdriver.chrome.driver="$DRIVER_PATH/chromedriver"\
- -Dwebdriver.chrome.logfile="chromedriver.log"\
- -Dphantomjs.binary.path="$DRIVER_PATH/phantomjs"\
- -Dphantomjs.cli.args="--webdriver-logfile=phantomjs.log"
+java -jar $BASEDIR/bin/selenium-server-standalone-2.42.0.jar\
+  -port 4455\
+  -Dwebdriver.chrome.driver="$DRIVER_PATH/chromedriver"\
+  -Dwebdriver.chrome.logfile="chromedriver.log"\
+  -Dphantomjs.binary.path="$DRIVER_PATH/phantomjs"\
+  -Dphantomjs.cli.args="--webdriver-logfile=phantomjs.log"
