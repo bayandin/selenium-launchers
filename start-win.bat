7af8093b38ffc7b69bec6589c92d47d1aba73ea7
i.pavlov <i.pavlov@2gis.ru>
2014-07-24 16:21:24 +0700

    add windows current directory path resolving

--- start-win.bat
+++ start-win.bat
@@ -1,19 +1,20 @@
 @echo off
-set /p VERSION=<selenium_version
+set CURRENT_DIR=%~dp0
+set /p VERSION=<%CURRENT_DIR%selenium_version
 
 IF %processor_architecture% == AMD64 (
-start java -jar bin\selenium-server-standalone-%VERSION%.jar^
+start java -jar %CURRENT_DIR%bin\selenium-server-standalone-%VERSION%.jar^
  -port 4455^
- -Dwebdriver.chrome.driver="bin\win\x86_64\chromedriver.exe"^
- -Dwebdriver.chrome.logfile="chromedriver.log"^
- -Dwebdriver.ie.driver="bin\win\x86-64\IEDriverServer.exe"^
- -Dphantomjs.binary.path="bin\win\x86-64\phantomjs.exe"
+ -Dwebdriver.chrome.driver="%CURRENT_DIR%bin\win\x86_64\chromedriver.exe"^
+ -Dwebdriver.chrome.logfile="%CURRENT_DIR%chromedriver.log"^
+ -Dwebdriver.ie.driver="%CURRENT_DIR%bin\win\x86-64\IEDriverServer.exe"^
+ -Dphantomjs.binary.path="%CURRENT_DIR%bin\win\x86-64\phantomjs.exe"
 )
 IF %processor_architecture% == x86 (
-start java -jar bin\selenium-server-%VERSION%.jar^
+start java -jar %CURRENT_DIR%bin\selenium-server-%VERSION%.jar^
  -port 4455^
- -Dwebdriver.chrome.driver="bin\win\x86\chromedriver.exe"^
- -Dwebdriver.chrome.logfile="chromedriver.log"^
- -Dwebdriver.ie.driver="bin\win\x86\IEDriverServer.exe"^
- -Dphantomjs.binary.path="bin\win\x86\phantomjs.exe"
+ -Dwebdriver.chrome.driver="%CURRENT_DIR%bin\win\x86\chromedriver.exe"^
+ -Dwebdriver.chrome.logfile="%CURRENT_DIR%chromedriver.log"^
+ -Dwebdriver.ie.driver="%CURRENT_DIR%bin\win\x86\IEDriverServer.exe"^
+ -Dphantomjs.binary.path="%CURRENT_DIR%bin\win\x86\phantomjs.exe"
 )
