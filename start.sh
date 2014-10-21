33a290fb682d7b4214324f9ca4acfd0932cf7648
i.pavlov <i.pavlov@2gis.ru>
2014-10-21 18:17:49 +0700

    fix java to use /dev/urandom on *nix systems

--- start.sh
+++ start.sh
@@ -4,6 +4,11 @@ MACHINE_OS=`uname`
 MACHINE_TYPE=`uname -m`
 BASEDIR=$(dirname $0)
 VERSION=$(cat ${BASEDIR}/selenium_version)
+JAVA_SWITCHES=""
+# By default java on linux uses /dev/random that can't provide enough entropy for ssd disks or virtual machines.
+# https://code.google.com/p/selenium/wiki/FrequentlyAskedQuestions#Q:_Selenium_server_sometimes_takes_a_long_time_to_start_a_new_se
+# https://code.google.com/p/selenium/issues/detail?id=1301
+JAVA_SWITCHES="$JAVA_SWITCHES -Djava.security.egd=file:///dev/urandom"
 
 if [ ${MACHINE_OS} == 'Linux' ]; then
   DRIVER_PATH="bin/linux"
@@ -16,7 +21,7 @@ fi
 # Add machine type to path
 DRIVER_PATH="$BASEDIR/$DRIVER_PATH/$MACHINE_TYPE"
 
-java -jar $BASEDIR/bin/selenium-server-standalone-$VERSION.jar\
+java $JAVA_SWITCHES -jar $BASEDIR/bin/selenium-server-standalone-$VERSION.jar\
   -port 4455\
   -Dwebdriver.chrome.driver="$DRIVER_PATH/chromedriver"\
   -Dwebdriver.chrome.logfile="chromedriver.log"\
